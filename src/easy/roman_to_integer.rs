c846e4ef6bab4b046f4f20a5bfc25ff5be9987b9
defnot001 <marc.miller994@gmail.com>
2023-06-03 22:28:12 +0200

    removed unnecessary comment

--- src/easy/roman_to_integer.rs
+++ src/easy/roman_to_integer.rs
@@ -3,16 +3,16 @@ pub fn roman_to_int(s: String) -> i32 {
     let mut largest_seen = 0;
 
     for ch in s.chars().rev() {
-         let num = match ch {
-             'I' => 1,
-             'V' => 5,
-             'X' => 10,
-             'L' => 50,
-             'C' => 100,
-             'D' => 500,
-             'M' => 1000,
-             _ => unreachable!(),
-         };
+        let num = match ch {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => unreachable!(),
+        };
 
         if num < largest_seen {
             sum -= num;
@@ -22,11 +22,5 @@ pub fn roman_to_int(s: String) -> i32 {
         }
     }
 
-     return sum;
+    return sum;
 }
-
-
-
-// MCMXCIV
-//
-// 5 - 1 + 100 - 10 + 1000 - 100 + 1000  
